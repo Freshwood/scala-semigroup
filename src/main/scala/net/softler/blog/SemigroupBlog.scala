40539840b4a6a7c99d15fa7673378de6098e14aa
Tobias Frischholz <tfrischholz@kabelmail.de>
2017-11-19 21:43:05 +0100

    Improved sample implementation

--- src/main/scala/net/softler/blog/SemigroupBlog.scala
+++ src/main/scala/net/softler/blog/SemigroupBlog.scala
@@ -19,11 +19,6 @@ object Groups {
   implicit object CarGroup extends Semigroup[Car] {
     override def append(a: Car, b: Car): Car = Car(a.horsePower + b.horsePower)
   }
-
-  implicit object IntIntGroup extends Semigroup[Int => Int] {
-    override def append(a: Int => Int, b: Int => Int): Int => Int =
-      input => a(input) + b(input)
-  }
 }
 
 case class Car(horsePower: Int)
@@ -36,38 +31,17 @@ object SemigroupBlog extends App {
 
   import Groups._
 
-  def calc[A](input: A)(implicit semigroup: Semigroup[A]): A =
+  def calculate[A](input: A)(implicit semigroup: Semigroup[A]): A =
     semigroup.append(input, input)
 
   def reduce[A](input: List[A])(implicit semigroup: Semigroup[A]): A =
     input.reduce(semigroup.append)
 
-  def associativity[A](a1: A, a2: A, a3: A)(implicit semigroup: Semigroup[A]): Boolean = {
-    val first = semigroup.append(a1, semigroup.append(a2, a3))
-    val second = semigroup.append(semigroup.append(a1, a2), a3)
-    println(first + "==" + second)
-    first == second
-  }
-
-  println(calc(100d))
+  println(calculate(100d))
 
   println(reduce(List(1, 2, 3, 4, 5)))
 
-  println(calc(Car(150)))
-
-  println(associativity(100, 50, 100))
+  println(calculate(Car(150)))
 
   println(reduce(List(Car(20), Car(50), Car(111))))
-
-  val fn: Int => Int = _ * 2
-
-  println {
-    val result = calc(fn)
-    result(10)
-  }
-
-  println {
-    val result = reduce(List(fn, fn, fn, fn, fn, fn, fn, fn))
-    result(1)
-  }
 }
